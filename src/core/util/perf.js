f105d29371af682ba4f337829ffeb8747b976ac6
gerrard_pan <gerrard_pan@yahoo.com>
2020-03-28 22:10:34 +0800

    add performance.js comment

--- src/core/util/perf.js
+++ src/core/util/perf.js
@@ -1,9 +1,11 @@
 import { inBrowser } from './env'
 
+// 到处简单封装之后的mark函数，以及修改过后的measure函数
 export let mark
 export let measure
 
 if (process.env.NODE_ENV !== 'production') {
+  // 若是浏览器环境，则把perf设置为performance对象
   const perf = inBrowser && window.performance
   /* istanbul ignore if */
   if (
@@ -13,8 +15,10 @@ if (process.env.NODE_ENV !== 'production') {
     perf.clearMarks &&
     perf.clearMeasures
   ) {
+    // 简单重新封装performance.mark()，以及measuer函数
     mark = tag => perf.mark(tag)
     measure = (name, startTag, endTag) => {
+      // 记录2个mark之间的时间差，并清除marks
       perf.measure(name, startTag, endTag)
       perf.clearMarks(startTag)
       perf.clearMarks(endTag)
